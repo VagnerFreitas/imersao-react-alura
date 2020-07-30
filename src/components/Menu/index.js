9e1c186d1b5243884c3fb82516b987d21e09611e
Vagner Freitas <vagner_freitas@hotmail.com>
2020-07-29 21:40:04 -0300

    Criado o projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+import {Link} from 'react-router-dom'
+
+function Menu(){
+    return (
+        <nav className='Menu'>
+            <Link to="/">
+                <img className = "Logo" src={Logo} alt="AluraFlix"/>
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
